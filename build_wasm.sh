b0946f517c702c08a657b399afdc6205dda53758
Devin Leamy <devinleamy@gmail.com>
2023-04-17 19:31:42 -0400

    Fixed FFI

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,2 +1,2 @@
 cargo build --release --target wasm32-unknown-unknown
-wasm-bindgen --out-dir ./out/ --target web ./target/wasm32-unknown-unknown/release/Hoops.wasm
+wasm-bindgen --out-dir ./hoops/ --target web ./target/wasm32-unknown-unknown/release/Hoops.wasm
--- src/wasm.rs
+++ src/wasm.rs
@@ -1,17 +1,11 @@
 use wasm_bindgen::prelude::*;
 
-pub static mut JS_POINT_AWARED_CALLBACK: Option<js_sys::Function> = None;
-
 // all javascript callbacks
 pub mod js {
     use super::*;
 
     pub fn on_point_awared() {
-        unsafe {
-            if let Some(callback) = &JS_POINT_AWARED_CALLBACK {
-                callback.call0(&JsValue::NULL).unwrap();
-            }
-        }
+        onBasket();
     }
 }
 
@@ -19,6 +13,7 @@ pub mod js {
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
+    fn onBasket();
 }
 
 #[wasm_bindgen]
@@ -28,13 +23,6 @@ pub fn marco() {
 }
 
 #[wasm_bindgen]
-pub fn hoops_init(point_awared_callback: &js_sys::Function) {
+pub fn hoops_init() {
     console_error_panic_hook::set_once();
-
-    // I don't _want_ to use unsafe here, but I don't see an
-    // obvious alternative.
-    // WARNING: Be careful about passing references!
-    unsafe {
-        JS_POINT_AWARED_CALLBACK = Some(point_awared_callback.clone());
-    }
 }
